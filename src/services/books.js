a6c35918463db68777861560c82516021eb0ca8a
ATarsa <agnieszkatarsa@gmail.com>
2020-07-12 16:34:50 +0100

    Use fetch API instead of axios

--- src/services/books.js
+++ src/services/books.js
@@ -1,15 +1,15 @@
-import axios from "axios";
-
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const getBookCover = async (title, author) => {
-  const res = await axios.get(
+async function getBookCover(title, author) {
+  const res = await fetch(
     `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}+inauthor:${author}&key=${API_KEY}`,
     {}
   );
+  const data = await res.json();
   // return img html
-  console.log(res.data.items[0].volumeInfo.imageLinks.thumbnail);
-  return res.data.items[0].volumeInfo.imageLinks.smallThumbnail;
-};
+  if (data.items) {
+    return data.items[0].volumeInfo.imageLinks.smallThumbnail;
+  }
+}
 
 export { getBookCover };
