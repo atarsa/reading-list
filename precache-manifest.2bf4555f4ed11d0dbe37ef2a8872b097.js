929e64062614fb2416e9b6b5017ea7562f4c667e
AT <agnieszkatarsa@gmail.com>
2020-07-14 17:44:12 +0100

    Updates

---
+++ precache-manifest.2bf4555f4ed11d0dbe37ef2a8872b097.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5fe04509fcec275c6113b68b7bbac9",
+    "url": "/atarsa/reading-list/index.html"
+  },
+  {
+    "revision": "e74208b9dab4211a0e2d",
+    "url": "/atarsa/reading-list/static/css/2.9340ea74.chunk.css"
+  },
+  {
+    "revision": "e74208b9dab4211a0e2d",
+    "url": "/atarsa/reading-list/static/js/2.c01a085b.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/atarsa/reading-list/static/js/2.c01a085b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559a152d8f46d95f5173",
+    "url": "/atarsa/reading-list/static/js/main.88234e4e.chunk.js"
+  },
+  {
+    "revision": "81746f962336f13d9ad2",
+    "url": "/atarsa/reading-list/static/js/runtime-main.c58d364d.js"
+  },
+  {
+    "revision": "24f7b0944e9e03a905f9d7701573b2cd",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-300.24f7b094.woff2"
+  },
+  {
+    "revision": "a7622f60c56ddd5301549a786b54e6e6",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-300.a7622f60.woff"
+  },
+  {
+    "revision": "6725c3fb1f3abd7ebdcb2d244758f706",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-300italic.6725c3fb.woff2"
+  },
+  {
+    "revision": "793b1237017aeacd646fb80911425566",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-300italic.793b1237.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-400.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-400.de0869e3.woff"
+  },
+  {
+    "revision": "06b4bfda4e139eaf3ab9872a6d66f42f",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-400italic.06b4bfda.woff"
+  },
+  {
+    "revision": "b61a9055d92ac56c15cacf4271d4f072",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-400italic.b61a9055.woff2"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "56e5756b696615d6164a625e1bcb1a9e",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-600italic.56e5756b.woff"
+  },
+  {
+    "revision": "ebdfa24cb411ba3016abd1c3ea5746e5",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-600italic.ebdfa24c.woff2"
+  },
+  {
+    "revision": "0edb76284a7a0f8db4665b560ee2b48f",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-700.0edb7628.woff2"
+  },
+  {
+    "revision": "1f85e92d8ff443980bc0f83ad7b23b60",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-700.1f85e92d.woff"
+  },
+  {
+    "revision": "a49393f2171a2f4d6927ce6c14f73dbc",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-700italic.a49393f2.woff2"
+  },
+  {
+    "revision": "bf72679ca22e53320beaea090e8bb07d",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-700italic.bf72679c.woff"
+  },
+  {
+    "revision": "05ebdbe10796850f045fcd484f35788d",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-800.05ebdbe1.woff"
+  },
+  {
+    "revision": "ab3e500e7375695d702cff19513e4470",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-800.ab3e500e.woff2"
+  },
+  {
+    "revision": "92da6f116d973bd334cf9b3afdb29c4f",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-800italic.92da6f11.woff"
+  },
+  {
+    "revision": "c62c3eea77cf3fca51494e8c924adf4d",
+    "url": "/atarsa/reading-list/static/media/open-sans-latin-800italic.c62c3eea.woff2"
+  },
+  {
+    "revision": "5cc7d7ee406e32dbaa00a811be2956f2",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-200.5cc7d7ee.woff"
+  },
+  {
+    "revision": "6cabbbc8becd23d64515a8cd6b50ad8a",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-200.6cabbbc8.woff2"
+  },
+  {
+    "revision": "34bd8be6a179b0547377d9399a45eaa0",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-300.34bd8be6.woff"
+  },
+  {
+    "revision": "650aac5096a5ffc76dc84e762acff206",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-300.650aac50.woff2"
+  },
+  {
+    "revision": "0e88ec239d6256e2c889df2e3f0d51f2",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-400.0e88ec23.woff"
+  },
+  {
+    "revision": "f3655d2e126fb0891c94407ce82bea4b",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-400.f3655d2e.woff2"
+  },
+  {
+    "revision": "5937c6cb6a7ad1219eca4204286dd3b2",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-500.5937c6cb.woff"
+  },
+  {
+    "revision": "b7ce0f303c0587004ba9590124f3e21b",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-500.b7ce0f30.woff2"
+  },
+  {
+    "revision": "6a98169ce7d291e352b05f574181e0bc",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-600.6a98169c.woff"
+  },
+  {
+    "revision": "c86f7df28ab1432da37c2276638fcf09",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-600.c86f7df2.woff2"
+  },
+  {
+    "revision": "7c915161ba6ba5f3f0b00c8f0708eaa9",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-700.7c915161.woff2"
+  },
+  {
+    "revision": "c93625b557a32ebeb334b05dab8e4728",
+    "url": "/atarsa/reading-list/static/media/oswald-latin-700.c93625b5.woff"
+  },
+  {
+    "revision": "d733be53ec37111d20e1097411794aa8",
+    "url": "/atarsa/reading-list/static/media/recycle-bin.d733be53.svg"
+  },
+  {
+    "revision": "7094e50c3856cbca434b00d90406e0cc",
+    "url": "/atarsa/reading-list/static/media/undraw_book_lover_mkck.7094e50c.svg"
+  }
+]);
